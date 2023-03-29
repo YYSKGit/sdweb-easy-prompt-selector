6baccaa937b590b02f1df36be5f450c671a1792d
YYSK-WORK <1440240857@qq.com>
2023-03-29 13:51:58 +0800

    Gradio 3.22 支持 V1

--- javascript/interactive_tag_selector.js
+++ javascript/interactive_tag_selector.js
@@ -1,210 +1,139 @@
-class InteractiveTagSelector {
+class ITSElementBuilder {
+  // Templates
+  static baseButton(text, { size = 'sm', color = 'primary' }) {
+    const button = gradioApp().getElementById('txt2img_generate').cloneNode()
+    button.id = ''
+    button.classList.remove('gr-button-lg', 'gr-button-primary', 'lg', 'primary')
+    button.classList.add(
+      // gradio 3.16
+      `gr-button-${size}`,
+      `gr-button-${color}`,
+      // gradio 3.22
+      size,
+      color
+    )
+    button.textContent = text
 
+    return button
+  }
+
+  static tagFields() {
+    const fields = document.createElement('div')
+    fields.style.display = 'flex'
+    fields.style.flexDirection = 'row'
+    fields.style.flexWrap = 'wrap'
+    fields.style.minWidth = 'min(320px, 100%)'
+    fields.style.maxWidth = '100%'
+    fields.style.flex = '1 calc(50% - 20px)'
+    fields.style.borderWidth = '1px'
+    fields.style.borderColor = 'var(--block-border-color,#374151)'
+    fields.style.borderRadius = 'var(--block-radius,8px)'
+    fields.style.padding = '8px'
+    fields.style.height = 'fit-content'
+
+    return fields
+  }
+
+  // Elements
+  static openButton({ onClick }) {
+    const button = ITSElementBuilder.baseButton('🔯タグを選択', { size: 'sm', color: 'secondary' })
+    button.style = 'margin-top: 0.5rem;'
+    button.addEventListener('click', onClick)
+
+    return button
+  }
+
+  static areaContainer(id = undefined) {
+    const container = gradioApp().getElementById('txt2img_results').cloneNode()
+    container.id = id
+    container.style.gap = 0
+    container.style.display = 'none'
+
+    return container
+  }
+
+  static tagButton({ title, onClick, onRightClick, color = 'primary' }) {
+    const button = ITSElementBuilder.baseButton(title, { color })
+    button.style.height = '2rem'
+    button.style.flexGrow = '0'
+    button.style.margin = '2px'
+
+    button.addEventListener('click', onClick)
+    button.addEventListener('contextmenu', onRightClick)
+
+    return button
+  }
+
+  static dropDown(id, options, { onChange }) {
+    const select = document.createElement('select')
+    select.id = id
+
+    // gradio 3.16
+    select.classList.add('gr-box', 'gr-input')
+
+    // gradio 3.22
+    select.style.color = 'var(--body-text-color)'
+    select.style.backgroundColor = 'var(--input-background-fill)'
+    select.style.borderColor = 'var(--block-border-color)'
+    select.style.borderRadius = 'var(--block-radius)'
+    select.style.margin = '2px'
+    select.addEventListener('change', (event) => { onChange(event.target.value) })
+
+    const none = ['なし']
+    none.concat(options).forEach((key) => {
+      const option = document.createElement('option')
+      option.value = key
+      option.textContent = key
+      select.appendChild(option)
+    })
+
+    return select
+  }
+
+  static checkbox(text, { onChange }) {
+    const label = document.createElement('label')
+    label.style.display = 'flex'
+    label.style.alignItems = 'center'
+
+    const checkbox = gradioApp().querySelector('input[type=checkbox]').cloneNode()
+    checkbox.addEventListener('change', (event) => {
+       onChange(event.target.checked)
+    })
+
+    const span = document.createElement('span')
+    span.style.marginLeft = 'var(--size-2, 8px)'
+    span.textContent = text
+
+    label.appendChild(checkbox)
+    label.appendChild(span)
+
+    return label
+  }
+}
+
+class InteractiveTagSelector {
   PATH_FILE = 'tmp/interactiveTagSelector.txt'
   AREA_ID = 'interactive-tag-selector'
   SELECT_ID = 'interactive-tag-selector-select'
   CONTENT_ID = 'interactive-tag-selector-content'
-  NSFW_MODE_ID = 'interactive-tag-selector-nsfw-mode'
-
-  MY_WHITE_CHARS = [' ', '\r', '\n']
-  MY_CLIPR_CHARS = [',', ':', '|', '(', '[', '{', '<']
-  MY_CLIPL_CHARS = [',', ':', '|', ')', ']', '}', '>']
+  TO_NEGATIVE_PROMPT_ID = 'interactive-tag-selector-to-negative-prompt'
 
   constructor(yaml, gradioApp) {
     this.yaml = yaml
     this.gradioApp = gradioApp
     this.visible = false
-    this.nsfwMode = false
-    this.promptFocus = null
+    this.toNegative = false
+    this.tags = undefined
+  }
+
+  async init() {
+    this.tags = await this.parseFiles()
   }
 
   async readFile(filepath) {
     const response = await fetch(`file=${filepath}?${new Date().getTime()}`);
 
     return await response.text();
-  }
-
-  createTagButtons(tags, prefix = '',isTop) {
-    if (Array.isArray(tags)) {
-      return tags.map((tag) => this.createTagButton(tag, tag, 'secondary'))
-    } else {
-      return Object.keys(tags).map((key) => {
-        const values = tags[key]
-        let randomKey = `${prefix}/${key}`
-
-        if (typeof values === 'string') {
-          if (key.endsWith('-NSFW')) {
-            key = key.substring(0, key.length - 5)
-          } else if (this.nsfwMode) {
-            return null
-          }
-          return this.createTagButton(key, values, 'secondary')
-        }
-
-        const mSize = isTop ? '100%' :'50%'
-        const group = document.createElement('div')
-        group.classList.add('gr-block', 'gr-box', 'relative', 'w-full', 'border-solid', 'border', 'border-gray-200', 'flex', 'flex-col', 'col', 'flex-wrap', 'gap-2', 'p-2')
-        group.style = `min-width: min(320px, 100%); flex-basis: ${mSize}; flex-grow: 1;`
-        randomKey = randomKey.replace(/-/g,'/')
-        group.append(this.createTagButton(key, `__${randomKey}__`))
-        group.insertAdjacentHTML('beforeend', '<div class="flex flex-col buttons"></div>')
-
-        const buttons = group.querySelector('.buttons')
-        buttons.classList.add('gr-block', 'gr-box', 'relative', 'w-full', 'flex', 'flex-wrap')
-        buttons.style = 'flex-direction: initial;'
-
-        this.createTagButtons(values, randomKey, false).forEach((button) => {
-          if (button !== null) {
-            buttons.appendChild(button)
-          }
-        })
-
-        return group
-      })
-    }
-  }
-
-  createTagButton(title, value, color = 'primary') {
-    const button = document.createElement('button')
-    button.classList.add('gr-button', 'gr-button-sm', `gr-button-${color}`)
-    button.style = 'height: 2rem; flex-grow: 0; margin: 2px;'
-    button.textContent = title
-    button.addEventListener('click', () => { this.addTag(value) })
-
-    return button
-  }
-
-  createTagArea(tags = {}) {
-    const tagArea = document.createElement('div')
-    tagArea.id = this.AREA_ID
-    tagArea.classList.add('flex', 'flex-col', 'relative', 'col', 'gr-panel')
-    tagArea.style = 'display: none;'
-
-    tagArea.innerHTML = `
-      <div class="flex flex-col relative col">
-        <div class="gr-block gr-box relative w-full border-solid border border-gray-200">
-          <div class="flex flex-row flex-wrap w-full gap-2" style="align-items: center;">
-            <select id="${this.SELECT_ID}" class="gr-box gr-input w-full" style="min-width: min(400px, 100%); flex: 3;">
-              <option>[无]</option>
-            </select>
-            <div style="min-width: min(200px, 100%); flex: 1">
-              <label class="flex items-center text-gray-700 text-sm space-x-2 rounded-lg cursor-pointer dark:bg-transparent">
-                <input type="checkbox" id="${this.NSFW_MODE_ID}" class="gr-check-radio gr-checkbox">
-                <span class="ml-2">🌸 NSFW</span>
-              </label>
-            </div>
-          </div>
-          <div id="${this.CONTENT_ID}" class="flex flex-row flex-wrap"></div>
-        </div>
-      </div>
-    `
-    const select = tagArea.querySelector(`#${this.SELECT_ID}`)
-    const content = tagArea.querySelector(`#${this.CONTENT_ID}`)
-    const nsfwModeCheckbox = tagArea.querySelector(`#${this.NSFW_MODE_ID}`)
-
-    select.addEventListener('change', (event) => {
-      this.selectTags(event.target.value, content)
-    })
-    nsfwModeCheckbox.addEventListener('change', (event) => {
-      this.nsfwMode = event.target.checked
-      this.updateTags(select, content, tags)
-    })
-    this.updateTags(select, content, tags)
-
-    gradioApp().getElementById('txt2img_toprow').after(tagArea)
-
-    const prompt = gradioApp().getElementById('txt2img_prompt').querySelector('textarea')
-    const promptNeg = gradioApp().getElementById('txt2img_neg_prompt').querySelector('textarea')
-    prompt.addEventListener('focus', () => {
-        this.promptFocus = prompt
-    })
-    promptNeg.addEventListener('focus', () => {
-        this.promptFocus = promptNeg
-    })
-    this.promptFocus = prompt
-  }
-
-  updateTags(select, content, tags) {
-    const selectValue = select.value
-    select.innerHTML = ''
-    content.innerHTML = ''
-
-    Object.keys(tags).forEach((key) => {
-      const values = tags[key]
-
-      const option = document.createElement('option')
-      option.value = key
-      option.textContent = key
-      select.appendChild(option)
-
-      const container = document.createElement('div')
-      container.id = `interactive-tag-selector-container-${key}`
-      container.classList.add('flex', 'flex-row', 'flex-wrap')
-      container.style = 'display: none;'
-
-      this.createTagButtons(values, key, true).forEach((group) => {
-        if (group !== null) {
-          container.appendChild(group)
-        }
-      })
-
-      content.appendChild(container)
-    })
-
-    if (selectValue !== '[无]') {
-      select.value = selectValue
-    }
-    this.selectTags(select.value, content)
-    select.focus({preventScroll: true})
-  }
-
-  selectTags(selected, content) {
-    Array.from(content.childNodes).forEach((node) => {
-      const visible = node.id === `interactive-tag-selector-container-${selected}`
-      this.changeVisibility(node, visible)
-    })
-  }
-
-  changeVisibility(node, visible) {
-    const style = visible ? 'display: flex;' : 'display: none;'
-    node.style = style
-  }
-
-  addTag(tag) {
-    const textArea = this.promptFocus
-    let mTextLeft = textArea.value.slice(0, textArea.selectionStart)
-    let mTextRight = textArea.value.slice(textArea.selectionEnd)
-    
-    for (let i = mTextLeft.length; i > 0; i --) {
-        let mTextClip = mTextLeft.slice(i - 1, i)
-        if (! this.MY_WHITE_CHARS.includes(mTextClip)) {
-            if (! this.MY_CLIPR_CHARS.includes(mTextClip)) {
-                tag = ', ' + tag
-            }
-            break
-        }
-    }
-
-    for (let i = 0; i < mTextRight.length; i ++) {
-        let mTextClip = mTextRight.slice(i, i + 1)
-        if (! this.MY_WHITE_CHARS.includes(mTextClip)) {
-            if (! this.MY_CLIPL_CHARS.includes(mTextClip)) {
-                tag = tag + ', '
-            }
-            break
-        }
-    }
-    if (mTextRight.length === 0) {
-        tag = tag + ', '
-    }
-
-    textArea.value = mTextLeft + tag + mTextRight
-    textArea.selectionStart = mTextLeft.length + tag.length
-    textArea.selectionEnd = textArea.selectionStart
-    updateInput(textArea)
-    
-    textArea.focus({preventScroll: true})
-    gradioApp().getElementById(this.SELECT_ID).focus({preventScroll: true})
   }
 
   async parseFiles() {
@@ -224,25 +153,158 @@ class InteractiveTagSelector {
 
     return tags
   }
+
+  // Render
+  render() {
+    const row = document.createElement('div')
+    row.style.display = 'flex'
+    row.style.alignItems = 'center'
+    row.style.gap = '10px'
+
+    const dropDown = this.renderDropdown()
+    dropDown.style.flex = '1'
+    dropDown.style.minWidth = '1'
+    row.appendChild(dropDown)
+
+    const settings = document.createElement('div')
+    const checkbox = ITSElementBuilder.checkbox('ネガティブプロンプトに入力', {
+      onChange: (checked) => { this.toNegative = checked }
+    })
+    settings.style.flex = '1'
+    settings.appendChild(checkbox)
+
+    row.appendChild(settings)
+
+    const container = ITSElementBuilder.areaContainer(this.AREA_ID)
+
+    container.appendChild(row)
+    container.appendChild(this.renderContent())
+
+    return container
+  }
+
+  renderDropdown() {
+    const dropDown = ITSElementBuilder.dropDown(
+      this.SELECT_ID,
+      Object.keys(this.tags), {
+        onChange: (selected) => {
+          const content = gradioApp().getElementById(this.CONTENT_ID)
+          Array.from(content.childNodes).forEach((node) => {
+            const visible = node.id === `interactive-tag-selector-container-${selected}`
+            this.changeVisibility(node, visible)
+          })
+        }
+      }
+    )
+
+    return dropDown
+  }
+
+  renderContent() {
+    const content = document.createElement('div')
+    content.id = this.CONTENT_ID
+
+    Object.keys(this.tags).forEach((key) => {
+      const values = this.tags[key]
+
+      const fields = ITSElementBuilder.tagFields()
+      fields.id = `interactive-tag-selector-container-${key}`
+      fields.style.display = 'none'
+      fields.style.flexDirection = 'row'
+      fields.style.marginTop = '10px'
+
+      this.renderTagButtons(values, key).forEach((group) => {
+        fields.appendChild(group)
+      })
+
+      content.appendChild(fields)
+    })
+
+    return content
+  }
+
+  renderTagButtons(tags, prefix = '') {
+    if (Array.isArray(tags)) {
+      return tags.map((tag) => this.renderTagButton(tag, tag, 'secondary'))
+    } else {
+      return Object.keys(tags).map((key) => {
+        const values = tags[key]
+        const randomKey = `${prefix}:${key}`
+
+        if (typeof values === 'string') { return this.renderTagButton(key, values, 'secondary') }
+
+        const fields = ITSElementBuilder.tagFields()
+        fields.style.flexDirection = 'column'
+
+        fields.append(this.renderTagButton(key, `@${randomKey}@`))
+
+        const buttons = ITSElementBuilder.tagFields()
+        buttons.id = 'buttons'
+        fields.append(buttons)
+        this.renderTagButtons(values, randomKey).forEach((button) => {
+          buttons.appendChild(button)
+        })
+
+        return fields
+      })
+    }
+  }
+
+  renderTagButton(title, value, color = 'primary') {
+    return ITSElementBuilder.tagButton({
+      title,
+      onClick: () => { this.addTag(value) },
+      onRightClick: (e) => { e.preventDefault(); this.removeTag(value) },
+      color
+    })
+  }
+
+  // Util
+  changeVisibility(node, visible) {
+    node.style.display = visible ? 'flex' : 'none'
+  }
+
+  addTag(tag) {
+    const id = this.toNegative ? 'txt2img_neg_prompt' : 'txt2img_prompt'
+    const textarea = gradioApp().getElementById(id).querySelector('textarea')
+
+    if (textarea.value.trim() !== '' && textarea.value.trim().slice(-1) !== ',') { textarea.value += ', ' }
+    textarea.value += tag
+
+    updateInput(textarea)
+  }
+
+  removeTag(tag) {
+    const id = this.toNegative ? 'txt2img_neg_prompt' : 'txt2img_prompt'
+    const textarea = gradioApp().getElementById(id).querySelector('textarea')
+
+    if (textarea.value.trimStart().startsWith(tag)) {
+      const matched = textarea.value.match(new RegExp(`${tag.replace(/[-\/\\^$*+?.()|\[\]{}]/g, '\\$&') },*`))
+      textarea.value = textarea.value.replace(matched[0], '').trimStart()
+    } else {
+      textarea.value = textarea.value.replace(`, ${tag}`, '')
+    }
+
+    updateInput(textarea)
+  }
 }
 
 onUiLoaded(async () => {
   yaml = window.jsyaml
   const interactiveTagSelector = new InteractiveTagSelector(yaml, gradioApp())
-  const tags = await interactiveTagSelector.parseFiles()
+  await interactiveTagSelector.init()
 
-  const button = document.createElement('button')
-  button.textContent = '🔯 提示词选择器'
-  button.classList.add('gr-button', 'gr-button-sm', 'gr-button-secondary')
-  button.style = 'margin-top: 0.5rem;'
-
-  button.addEventListener('click', () => {
-    const tagArea = gradioApp().querySelector(`#${interactiveTagSelector.AREA_ID}`)
-    interactiveTagSelector.changeVisibility(tagArea, interactiveTagSelector.visible = !interactiveTagSelector.visible)
+  const button = ITSElementBuilder.openButton({
+    onClick: () => {
+      const tagArea = gradioApp().querySelector(`#${interactiveTagSelector.AREA_ID}`)
+      interactiveTagSelector.changeVisibility(tagArea, interactiveTagSelector.visible = !interactiveTagSelector.visible)
+    }
   })
 
   const txt2imgActionColumn = gradioApp().getElementById('txt2img_actions_column')
   txt2imgActionColumn.appendChild(button)
 
-  interactiveTagSelector.createTagArea(tags)
+  gradioApp()
+    .getElementById('txt2img_toprow')
+    .after(interactiveTagSelector.render())
 })
